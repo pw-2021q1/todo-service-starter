78f2bea5641cc6fb97b8df9dfcccb78aa0a8b7aa
Prof. Diogo S. Martins <santana.martins@ufabc.edu.br>
2021-03-31 18:40:06 -0300

    initial commit

--- conf/config.ts
+++ conf/config.ts
@@ -3,7 +3,6 @@
 export const config = {
     db: {
         url: "mongodb://localhost:27017",
-        // "url": "mongodb+srv://mongodev:kh17wnrg2cFWoI12@cluster0.yifvq.mongodb.net",
         name: "todo-api", 
         collections: {
             todoItems: "todo-items",
